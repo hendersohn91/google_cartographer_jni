277e83513a28360418d64230ffd5b5e7fe1ed924
Eric Hähner <eric.haehner@gmail.com>
2020-05-17 16:46:27 +0200

    add gnuplot command to make sh

--- tests/cartographer_jni_cpp_test/make.sh
+++ tests/cartographer_jni_cpp_test/make.sh
@@ -5,14 +5,28 @@ SCRIPT_PATH=$GIT_ROOT_DIR/tests/cartographer_jni_cpp_test
 NATIVE_JAR='cartographer-platform-1.0.0-natives-linux.jar'
 cd $SCRIPT_PATH
 rm -rf build/
+# extract build files if not existing
+cd $GIT_ROOT_DIR/tests/test_data
+if [ ! -f "lrfdata.txt" ]; then
+	tar xvzf data.tar.gz run1_lrf.txt run1_odo.txt
+	mv run1_lrf.txt lrfdata.txt
+	mv run1_odo.txt odomdata.txt
+fi
+#build
+cd $SCRIPT_PATH
 if [ ! -d "build" ]; then
 	mkdir build
 fi
 cd build
+# extract necessary library
 jar xvf $GIT_ROOT_DIR/$NATIVE_JAR
 cmake ..
 make
+
+# execute
 ./cartographer_jni_cpp_test
+#plot
+gnuplot -e 'set palette defined ( -1 "#000000", 0 "#00FF00", 50 "#0000FF", 100 "#FF0000" ); set size ratio -1; plot "grid_cpp.txt" u 1:2:3 ls 5 lc palette z' -p
 
 # paint grid with gnuplot
 # data structure:
--- tests/cartographer_jni_test/make.sh
+++ tests/cartographer_jni_test/make.sh
@@ -9,7 +9,7 @@ GIT_ROOT_DIR=`git rev-parse --show-toplevel`
 SCRIPT_PATH=$GIT_ROOT_DIR/tests/cartographer_jni_test
 
 echo
-echo 'Created by Rui-Jie Fang, modified by Eric Hähner'
+echo 'Created by Eric Hähner'
 echo 'https://github.com/thefangbear/JNI-By-Examples'
 echo 'Options: ./make.sh'
 echo 'Note: Cartographer JNI jars should be created and copied to this project before executing this script'
@@ -31,11 +31,21 @@ build_project () {
 }
 
 execute_test () {
+	# extract build files if not existing
+	cd $GIT_ROOT_DIR/tests/test_data
+	if [ ! -f "lrfdata.txt" ]; then
+		tar xvzf data.tar.gz run1_lrf.txt run1_odo.txt
+		mv run1_lrf.txt lrfdata.txt
+		mv run1_odo.txt odomdata.txt
+	fi
+	cd $SCRIPT_PATH
     START=$(date +%s.%N)
     java -cp ".:$GIT_ROOT_DIR/$JAVA_LIB_JAR" $TEST_CLASS_NAME -Djava.library.path=$SCRIPT_PATH/build
     END=$(date +%s.%N)
     DIFF=$(echo "$END - $START" | bc)
     echo "time needed: $DIFF s"
+    #plot
+	gnuplot -e 'set palette defined ( -1 "#000000", 0 "#00FF00", 50 "#0000FF", 100 "#FF0000" ); set size ratio -1; plot "grid_java.txt" u 1:2:3 ls 5 lc palette z' -p
 }
 
 cleanup () {
